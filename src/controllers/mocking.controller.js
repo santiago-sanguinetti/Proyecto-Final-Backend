d184c4b9c183830e1bf9a28ea1d36e94b2214fcd
santiago-sanguinetti <sanguinettisantiago@gmail.com>
2023-11-12 21:54:50 -0300

    feat: mocking logs

--- src/controllers/mocking.controller.js
+++ src/controllers/mocking.controller.js
@@ -1,9 +1,12 @@
+import { logger } from "../config/logger.config.js";
 import { generateProduct } from "../utils.js";
 
 export const generate100Products = async (req, res) => {
+    logger.info("Generando 100 productos");
     let products = [];
     for (let i = 0; i < 100; i++) {
         products.push(generateProduct());
     }
+    logger.info("Productos generados exitosamente");
     res.send({ status: "success", payload: products });
 };
