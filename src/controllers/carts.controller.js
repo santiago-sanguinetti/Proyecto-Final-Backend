c3de1c1c4a750d0d61bc25d7b91a31cffe2e460a
santiago-sanguinetti <sanguinettisantiago@gmail.com>
2023-11-12 21:11:13 -0300

    feat: carts logs

--- src/controllers/carts.controller.js
+++ src/controllers/carts.controller.js
@@ -10,12 +10,14 @@ import {
     insufficientStock,
 } from "../services/errors/info.js";
 import mongoose from "mongoose";
+import { logger } from "../config/logger.config.js";
 const cartsManager = new Carts();
 const productsManager = new Products();
 const ticketsManager = new Tickets();
 
 //Crear un carrito
 export const createCart = async (req, res, next) => {
+    logger.info("Creando un nuevo carrito");
     const cart = {
         products: req.body.products,
     };
@@ -31,16 +33,20 @@ export const createCart = async (req, res, next) => {
                 message: "Los parámetros son inválidos",
                 code: EErrors.INVALID_TYPES_ERROR,
             });
+            logger.error(`Error al crear el carrito: ${error.message}`);
             return next(error);
         }
 
+        logger.info("Carrito creado exitosamente");
         res.status(201).json(newCart);
     } catch (err) {
+        logger.error(`Error al crear el carrito: ${err.message}`);
         return next(err);
     }
 };
 //Agregar un producto al carrito seleccionado
 export const addProductToCart = async (req, res, next) => {
+    logger.info("Agregando un producto al carrito");
     try {
         const cart = await cartsManager.getBy({ _id: req.params.cid });
         if (cart == null) {
@@ -50,6 +56,9 @@ export const addProductToCart = async (req, res, next) => {
                 message: "No se pudo encontrar el carrito",
                 code: EErrors.DATABASE_ERROR,
             });
+            logger.error(
+                `Error al agregar producto al carrito: ${error.message}`
+            );
             return next(error);
         }
 
@@ -59,20 +68,27 @@ export const addProductToCart = async (req, res, next) => {
         if (productIndex > -1) {
             // Si el producto ya existe en el carrito, incrementa la cantidad
             cart.products[productIndex].quantity += 1;
+            logger.info(
+                "El producto ya existe en el carrito, incrementando la cantidad"
+            );
         } else {
             // Si el producto no existe en el carrito, lo agrega
             cart.products.push({ productId: req.params.pid, quantity: 1 });
+            logger.info("El producto no existe en el carrito, agregándolo");
         }
 
         const updatedCart = await cartsManager.saveCart(cart);
+        logger.info("Producto agregado al carrito exitosamente");
         res.json(updatedCart);
     } catch (err) {
+        logger.error(`Error al agregar producto al carrito: ${err.message}`);
         return next(err);
     }
 };
 
 //Obtener un carrito por su id
 export const getCartById = async (req, res, next) => {
+    logger.info("Obteniendo carrito por ID");
     const { cid } = req.params;
     try {
         // const cart = await cartModel.findById(cid).populate("products");
@@ -84,17 +100,21 @@ export const getCartById = async (req, res, next) => {
                 message: "No se pudo encontrar el carrito",
                 code: EErrors.DATABASE_ERROR,
             });
+            logger.error(`Error al obtener el carrito: ${error.message}`);
             return next(error);
         }
         const populatedCart = await cartsManager.populateProducts(cart);
+        logger.info("Carrito obtenido con éxito");
         res.json({ populatedCart });
     } catch (err) {
+        logger.error(`Error al obtener el carrito: ${err.message}`);
         return next(err);
     }
 };
 
 //Eliminar del carrito un producto
 export const deleteProductFromCart = async (req, res, next) => {
+    logger.info("Eliminando producto del carrito");
     const { cid, pid } = req.params;
     try {
         const cart = await cartsManager.getBy({ _id: cid });
@@ -105,23 +125,29 @@ export const deleteProductFromCart = async (req, res, next) => {
                 message: "No se pudo encontrar el carrito",
                 code: EErrors.DATABASE_ERROR,
             });
+            logger.error(
+                `Error al eliminar producto del carrito: ${error.message}`
+            );
             return next(error);
         }
         cart.products = cart.products.filter(
             (product) => product._id.toString() !== pid
         );
         await cartsManager.saveCart(cart);
+        logger.info("Producto eliminado del carrito exitosamente");
         res.json({
             status: "success",
             message: "Producto eliminado del carrito",
         });
     } catch (err) {
+        logger.error(`Error al eliminar producto del carrito: ${err.message}`);
         return next(err);
     }
 };
 
 //Actualizar el carrito con array de productos
 export const updateCartFromArray = async (req, res, next) => {
+    logger.ingo("Actualizando carrito desde un array");
     const { cid } = req.params;
     const products = req.body.products;
     try {
@@ -133,6 +159,7 @@ export const updateCartFromArray = async (req, res, next) => {
                 message: "No se pudo encontrar el carrito",
                 code: EErrors.DATABASE_ERROR,
             });
+            logger.error(`Error al actualizar el carrito: ${error.message}`);
             return next(error);
         }
 
@@ -146,18 +173,22 @@ export const updateCartFromArray = async (req, res, next) => {
                 message: "Los parámetros son inválidos",
                 code: EErrors.INVALID_TYPES_ERROR,
             });
+            logger.error(`Errir al actualizar el carrito: ${error.message}`);
             return next(error);
         }
 
         await cartsManager.saveCart(cart);
+        logger.info("Carrito actualizado exitosamente");
         res.json({ status: "success", message: "Carrito actualizado" });
     } catch (err) {
+        logger.error(`Error al actualizar el carrito: ${err.message}`);
         return next(err);
     }
 };
 
 //Actualizar la cantidad de un producto en el carrito
 export const updateCartProductQuantity = async (req, res, next) => {
+    logger.info("Actualizando la cantidad de un producto en el carrito");
     const { cid, pid } = req.params;
     const quantity = req.body.quantity;
     try {
@@ -169,6 +200,9 @@ export const updateCartProductQuantity = async (req, res, next) => {
                 message: "No se pudo encontrar el carrito",
                 code: EErrors.DATABASE_ERROR,
             });
+            logger.error(
+                `Error al actualizar la cantidad de un producto: ${error.message}`
+            );
             return next(error);
         }
 
@@ -186,10 +220,14 @@ export const updateCartProductQuantity = async (req, res, next) => {
                     message: "Los parámetros son inválidos",
                     code: EErrors.INVALID_TYPES_ERROR,
                 });
+                logger.error(
+                    `Error al actualizar la cantidad de un producto: ${error.message}`
+                );
                 return next(error);
             }
 
             await cartsManager.saveCart(cart);
+            logger.info("Cantidad de producto actualizada exitosamente");
             res.json({
                 status: "success",
                 message: "Cantidad de producto actualizada",
@@ -201,15 +239,22 @@ export const updateCartProductQuantity = async (req, res, next) => {
                 message: "Producto no encontrado en el carrito",
                 code: EErrors.DATABASE_ERROR,
             });
+            logger.error(
+                `Error al actualizar la cantidad de un producto: ${error.message}`
+            );
             return next(error);
         }
     } catch (err) {
+        logger.error(
+            `Error al actualizar la cantidad de un producto: ${err.message}`
+        );
         return next(err);
     }
 };
 
 //Eliminar todos los productos del carrito
 export const clearCart = async (req, res, next) => {
+    logger.info("Vaciando el carrito");
     const { cid } = req.params;
     try {
         const cart = await cartsManager.getBy({ _id: cid });
@@ -220,6 +265,7 @@ export const clearCart = async (req, res, next) => {
                 message: "No se pudo encontrar el carrito",
                 code: EErrors.DATABASE_ERROR,
             });
+            logger.error(`Error al vaciar el carrito: ${error.message}`);
             return next(error);
         }
 
@@ -233,21 +279,25 @@ export const clearCart = async (req, res, next) => {
                 message: "Los parámetros son inválidos",
                 code: EErrors.INVALID_TYPES_ERROR,
             });
+            logger.error(`Error al vaciar el carrito: ${error.message}`);
             return next(error);
         }
 
         await cartsManager.saveCart(cart);
+        logger.info("Carrito vaciado exitosamente");
         res.json({
             status: "success",
             message: "Todos los productos eliminados del carrito",
         });
     } catch (err) {
+        logger.error(`Error al vaciar el carrito: ${err.message}`);
         return next(err);
     }
 };
 
 //Completar compra y generar ticket
 export const completePurchase = async (req, res, next) => {
+    logger.info("Completando la compra");
     try {
         // Obtiene el id del carrito de la solicitud
         const cartId = req.params.cid;
@@ -265,6 +315,7 @@ export const completePurchase = async (req, res, next) => {
                 message: "El carrito está vacío",
                 code: EErrors.EMPTY_CART_ERROR,
             });
+            logger.error(`Error al completar la compra: ${error.message}`);
             return next(error);
         }
 
@@ -298,6 +349,7 @@ export const completePurchase = async (req, res, next) => {
                 message: "No hay suficiente stock para algunos productos",
                 code: EErrors.INSUFFICIENT_STOCK_ERROR,
             });
+            logger.error(`Error al completar la compra: ${error.message}`);
             return next(error);
         }
 
@@ -310,18 +362,21 @@ export const completePurchase = async (req, res, next) => {
 
         // Crea un ticket para la compra
         ticketsManager.createPurchaseTicket(purchaseDetails);
-
+        logger.info("Compra finalizada exitosamente");
         res.status(200).json({ message: "Compra finalizada con éxito" });
-    } catch (error) {
+    } catch (err) {
+        logger.error(`Error al completar la compra: ${err.message}`);
         return next(err);
     }
 };
 
 const checkStock = async (product, productQuantity) => {
+    logger.debug(`Verificando stock del producto ${product._id}`);
     return product.stock >= productQuantity;
 };
 
 function validateCart(cart) {
+    logger.debug("Validando el carrito");
     if (!Array.isArray(cart.products)) {
         return "Los productos deben ser un array";
     }
