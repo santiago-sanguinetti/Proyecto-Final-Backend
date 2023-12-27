1f9de86735907c9fac0d3994a698d887642c52b2
santiago-sanguinetti <sanguinettisantiago@gmail.com>
2023-12-27 01:05:05 -0300

    feat: added name in users DTO

--- src/dao/DTOs/user.dto.js
+++ src/dao/DTOs/user.dto.js
@@ -1,6 +1,8 @@
 export default class UserDTO {
     constructor(user) {
         (this._id = user._id),
+            (this.first_name = user.first_name),
+            (this.last_name = user.last_name),
             (this.email = user.email),
             (this.role = user.role);
     }
@@ -8,6 +10,8 @@ export default class UserDTO {
     toJSON() {
         return {
             _id: this._id,
+            first_name: this.first_name,
+            last_name: this.last_name,
             email: this.email,
             role: this.role,
         };
