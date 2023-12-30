206f4f2d08ea72cb68a9179b93292d34c5861291
santiago-sanguinetti <sanguinettisantiago@gmail.com>
2023-12-30 17:20:10 -0300

    feat: userDTO now displays user age

--- src/dao/DTOs/user.dto.js
+++ src/dao/DTOs/user.dto.js
@@ -4,8 +4,9 @@ export default class UserDTO {
             (this.first_name = user.first_name),
             (this.last_name = user.last_name),
             (this.email = user.email),
-            (this.role = user.role);
-        this.cart = user.cart;
+            (this.role = user.role),
+            (this.age = user.age),
+            (this.cart = user.cart);
     }
 
     toJSON() {
