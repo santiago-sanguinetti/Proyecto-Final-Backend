8ab4cac72f05e81de1a30f93abbf4413b71722a5
santiago-sanguinetti <sanguinettisantiago@gmail.com>
2023-12-30 15:23:11 -0300

    refactor: show user cart in user DTO

--- src/dao/DTOs/user.dto.js
+++ src/dao/DTOs/user.dto.js
@@ -5,6 +5,7 @@ export default class UserDTO {
             (this.last_name = user.last_name),
             (this.email = user.email),
             (this.role = user.role);
+        this.cart = user.cart;
     }
 
     toJSON() {
@@ -14,6 +15,7 @@ export default class UserDTO {
             last_name: this.last_name,
             email: this.email,
             role: this.role,
+            cart: this.cart,
         };
     }
 }
